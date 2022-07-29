a030e0ae1848a24aac6c3c91dcd06410e2887322
priyanshu <priyanshubhardwaj1422@gmail.com>
2022-07-30 00:19:59 +0530

    find cycle in link list

---
+++ q_141.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+  bool hasCycle(ListNode *head)
+  {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    if (head == NULL)
+    {
+      return false;
+    }
+    while (fast != NULL && fast->next != NULL)
+    {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (fast == slow)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+};

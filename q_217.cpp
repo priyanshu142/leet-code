16443fc46a136bf44856f2b387637bbaa8d66aa9
priyanshu <priyanshubhardwaj1422@gmail.com>
2022-08-05 23:35:21 +0530

    contains's duplicate

---
+++ q_217.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+  bool containsDuplicate(vector<int> &nums)
+  {
+    map<int, int> mp;
+    for (auto it : nums)
+    {
+      mp[it]++;
+    }
+    for (auto it : mp)
+    {
+      if (it.second >= 2)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+};

2230e407a8e956025d5caef403382902c2b58287
priyanshu <priyanshubhardwaj1422@gmail.com>
2022-09-02 23:53:45 +0530

    delete node in a bst

---
+++ q_450.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+public:
+  TreeNode *findMIN(TreeNode *root)
+  {
+    while (root->left != NULL)
+    {
+      if (root->left == NULL)
+      {
+        return root;
+      }
+      root = root->left;
+    }
+    return root;
+  }
+  TreeNode *del(TreeNode *root, int key)
+  {
+    if (root == NULL)
+    {
+      return root;
+    }
+    else if (key < root->val)
+    {
+      root->left = del(root->left, key);
+    }
+    else if (key > root->val)
+    {
+      root->right = del(root->right, key);
+    }
+    else
+    {
+      if (root->left == NULL && root->right == NULL)
+      {
+        root = NULL;
+        return root;
+      }
+      else if (root->left == NULL)
+      {
+        TreeNode *temp = root;
+        root = root->right;
+        return root;
+      }
+      else
+      {
+        TreeNode *temp = findMIN(root->right);
+        root->val = temp->val;
+        root->right = del(root->right, temp->val);
+      }
+    }
+    return root;
+  }
+
+  TreeNode *deleteNode(TreeNode *root, int key)
+  {
+    root = del(root, key);
+    return root;
+  }
+};
